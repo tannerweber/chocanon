f8b50f0c6818a491a0f6d4bcb27d36d3721ead67
Tanner Weber <tannerw@pdx.edu>
2025-10-30 00:13:05 -0700

    Fixed dates

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
  * - Miles Turoczy, tcostal2@pdx.edu
  *
  * Portland State University
- * Dates October 29 to December 5
+ * Dates: October 29 to December 5
  * Course: CS 314, Fall 2025
  * Instructor: Christopher Gilmore
  */
