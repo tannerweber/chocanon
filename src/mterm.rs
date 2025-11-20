cb5d4458d553c30235bf7c45b03a4d4963eafc62
Torin <tcostal2@pdx.edu>
2025-11-19 16:11:46 -0800

    mterm changes

--- src/mterm.rs
+++ src/mterm.rs
@@ -14,3 +14,51 @@
  */
 
 //! Module for the manager terminal.
+use crate::db::DB;
+use color_eyre::install;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use std::io;
+
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, List, ListItem, Paragraph, Widget},
+};
+
+#[derive(Debug, Default)]
+pub struct App {
+    choice: u8,
+    input: String,
+    character_index: usize,
+    exit: bool,
+}
+
+impl App {
+    pub fn run_term(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+    ) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+    fn draw(&self, frame: &mut Frame) {
+        todo!()
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
+    }
+}
+fn run_man_term() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run_term(&mut terminal);
+    ratatui::restore();
+    app_result
+}
