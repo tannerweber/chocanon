88b465bb2cba6dc8adeaffc70bc8cbee70421888
Tanner Weber <tannerw@pdx.edu>
2025-12-04 23:49:18 -0800

    feat: populate database option

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@
 
 //! Chocaholics Anonymous project.
 
-use chocanon::db::DB;
+use chocanon::db::{Consultation, DB, LocationInfo, PersonInfo};
 use chocanon::{mterm, pterm};
 use std::io::{self, Write};
 
@@ -24,6 +24,7 @@ enum MenuOption {
     Quit,
     ProviderTerminal,
     ManagerTerminal,
+    PopulateDatabase,
 }
 
 const DB_PATH: &str = "./chocanon.db3";
@@ -49,6 +50,10 @@ fn main() {
                 println!("Chose manager terminal");
                 mterm::run_man_term(&db);
             }
+            MenuOption::PopulateDatabase => {
+                println!("Populating database");
+                populate_database(&db);
+            }
         }
     }
 }
@@ -60,6 +65,7 @@ Enter a number corresponding to the option
 ( 0 ) Quit
 ( 1 ) Provider Terminal
 ( 2 ) Manager Terminal
+( 3 ) Populate database with some values
 Choice: "
     );
     io::stdout().flush().expect("Failed to flush stdout");
@@ -92,8 +98,56 @@ fn get_valid_user_input() -> MenuOption {
         return MenuOption::ProviderTerminal;
     }
     if number == 2 {
-        MenuOption::ManagerTerminal
+        return MenuOption::ManagerTerminal;
+    }
+    if number == 3 {
+        MenuOption::PopulateDatabase
     } else {
         MenuOption::Quit
     }
+}
+
+fn populate_database(db: &DB) {
+    fn create_a_unique_person(name: &str, id: u32) -> PersonInfo {
+        let location =
+            LocationInfo::new("1234 Main st", "Portland", "OR", 56789).unwrap();
+        let email = format!("{}@pdx.edu", name);
+        PersonInfo::new(name, id, &location, &email).unwrap()
+    }
+
+    /// Creates a consultation with a date of yesterday.
+    fn create_a_unique_consultation(
+        member_id: u32,
+        provider_id: u32,
+    ) -> Consultation {
+        let date = (chrono::Local::now() - chrono::Duration::days(1))
+            .format("%m-%d-%Y")
+            .to_string();
+        let date_time = (chrono::Local::now() - chrono::Duration::days(1))
+            .format("%m-%d-%Y %H:%M:%S")
+            .to_string();
+        Consultation::new(
+            &date_time,
+            &date,
+            provider_id,
+            member_id,
+            123456,
+            "This is a comment created by create_a_unique_consultation",
+        )
+        .unwrap()
+    }
+
+    let _ = db.add_service(123456, "ServiceName123456", 99.99);
+    let _ = db.add_member(&create_a_unique_person("MemberName1", 1));
+    let _ = db.add_member(&create_a_unique_person("MemberName2", 2));
+    let _ = db.add_member(&create_a_unique_person("MemberName3", 3));
+    let _ = db.add_member(&create_a_unique_person("MemberName4", 4));
+    let _ = db.add_provider(&create_a_unique_person("ProviderName1", 1));
+    let _ = db.add_provider(&create_a_unique_person("ProviderName2", 2));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(1, 1));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(2, 1));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(2, 1));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(3, 1));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(3, 1));
+    let _ = db.add_consultation_record(&create_a_unique_consultation(3, 1));
 }
